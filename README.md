217bdccf8d4213c64304af22aa58263448019895
raulFuzita <raul_fuzita@hotmail.com>
2021-03-23 00:31:26 +0000

    ******* WHAT'S NEW? ******
    IMPROVEMENTS
    
    . starwars repo uses enviroment variables (env) to set server port.

--- README.md
+++ README.md
@@ -3,4 +3,7 @@ This is simple project that shows how to use Star Wars API with MVC.
 
 Please when you clone this repository, enter command <code>npm install</code>
 
+This branch of the starwars repo uses file .env. Please, open the file .env example and read the instruction.
+
 You can run the code by typing <code>node app</code> or <code>nodemon start</code>
+
--- app.js
+++ app.js
@@ -2,8 +2,18 @@ const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
 const engines = require('consolidate'); // Simple engine to render html from views folder
+const dotenv = require('dotenv'); // Load variables from file .env
 
+/**
+ * ################ IMPORTANT #################
+ * 
+ * Dotenv will retrive the port from file .env
+ * make sure you read the instructions form
+ * the file .env example. 
+ */
+dotenv.config();
 const app = express();
+const port = process.env.SERVER_PORT; // SERVER_PORT variable comes from file .env
 // Import a router
 const router = require('./routes/starWarsRoutes');
 // Set a basic engine to render an HTML file from views folder
@@ -29,7 +39,7 @@ app.get('/', (req, res) => {
     res.redirect('/index');
 }); // root url
 
-// Start listening on port 3000
-app.listen(8000, () => {
-    console.log('Server running on port 8000');
+// Start listening on port 8000
+app.listen(port, () => {
+    console.log('Server running on port ' + port);
 });
